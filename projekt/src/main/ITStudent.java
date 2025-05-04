aad1147c7829b58fec7156a3c412405d52174ea4
nklids <270885@vut.cz>
2025-05-04 13:56:54 +0200

    Add files via upload

---
+++ projekt/src/main/ITStudent.java
@@ -0,0 +1,31 @@
+package main;
+
+public class ITStudent extends Student {
+
+    public ITStudent(int id, String jmeno, String prijmeni, int rokNarozeni) {
+        super(id, jmeno, prijmeni, rokNarozeni);
+    }
+
+    @Override
+    public void spustDovednost() {
+        System.out.println("Morseova abeceda: " + doMorse(jmeno + " " + prijmeni));
+    }
+
+    private String doMorse(String vstup) {
+        String[] morse = {
+            ".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---",
+            "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-",
+            "..-", "...-", ".--", "-..-", "-.--", "--.."
+        };
+        vstup = vstup.toUpperCase();
+        StringBuilder sb = new StringBuilder();
+        for (char c : vstup.toCharArray()) {
+            if (Character.isLetter(c)) {
+                sb.append(morse[c - 'A']).append(" ");
+            } else if (c == ' ') {
+                sb.append(" / ");
+            }
+        }
+        return sb.toString();
+    }
+}
